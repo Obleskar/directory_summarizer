bd37d58fc99db2f645ff16436167131695baacb1
Obleskar <obleskar@gmail.com>
2018-09-13 15:18:02 -0700

    Print: Add progress statements.

--- cli.py
+++ cli.py
@@ -6,10 +6,10 @@ from pprint import pprint
 
 
 @click.command()
-@click.option('--target_path', '-tp', help='The path of the directory to be summarized..')
+@click.option('--target_path', '-p', help='The path of the directory to be summarized..')
 def main(target_path):
     """Summarize the contents of a given directory."""
-    print('Reading files...')
+    print('Reading directory.')
     summary = {'name': [],
                'size': [],
                'file_count': [],
@@ -25,10 +25,14 @@ def main(target_path):
         summary['size'].append(dir_sizes[directory_path])
         summary['file_count'].append(len(files))
         summary['subdirectories'].append(', '.join([directory for directory in subdirs]))
+    print('Formatting size values.')
     summary['size'] = [sizeof_fmt(size) for size in summary['size']]
+    print('Building spreadsheet.')
     dataframe = DataFrame(data=summary)
+    print('Writing results to CSV file.')
     with open('summary.csv', 'w') as outfile:
         dataframe.to_csv(outfile, index=False)
+    print('Your file\'s ready. Look for \"summary.csv\" in the current folder.')
 
 
 def sizeof_fmt(num, suffix='B'):
