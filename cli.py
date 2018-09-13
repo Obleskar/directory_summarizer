9b18c81a8e0ae93df396550cbe9a76a2a60efd18
Obleskar <obleskar@gmail.com>
2018-09-13 14:58:13 -0700

    Put the discovered values into a local CSV.

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import click
 from os import getcwd, path, walk
+from pandas import DataFrame
 from pprint import pprint
 
 
@@ -13,18 +14,24 @@ def main(target_path):
                'size': [],
                'file_count': [],
                'subdirectories': []}
-    total_size = 0
-    for directory, subdirectories, files in walk(top='resources/', topdown=False):
-        directory_size = sum(size for size in [path.getsize(file_path)
-                                               for file_path in [path.join(directory, file_name)
+    dir_sizes = {}
+    for directory_path, subdirs, files in walk(top='resources/', topdown=False):
+        size_files = sum(size for size in [path.getsize(file_path)
+                                               for file_path in [path.join(directory_path, file_name)
                                                             for file_name in files]])
-        total_size += directory_size
-        summary['name'].append(directory)
-        summary['size'].append(path.getsize(directory))
+        size_subdirs = sum(dir_sizes[path.join(directory_path, subdirectory)] for subdirectory in subdirs)
+        dir_sizes[directory_path] = size_files + size_subdirs
+        summary['name'].append(directory_path)
+        summary['size'].append(dir_sizes[directory_path])
         summary['file_count'].append(len(files))
-        summary['subdirectories'].append(', '.join([directory for directory in subdirectories]))
+        summary['subdirectories'].append(', '.join([directory for directory in subdirs]))
+    # summary['size'] = [size / 1024 for size in summary['size']]
     print('Summary:')
     pprint(summary)
+    dataframe = DataFrame(data=summary)
+    with open('summary.csv', 'w') as outfile:
+        dataframe.to_csv(outfile, index=False)
+    print(dataframe)
 
 
 if __name__ == '__main__':
