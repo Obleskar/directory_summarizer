e6a42b96496b8d4a366d24e4b9b9f7b4b4284e00
Obleskar <obleskar@gmail.com>
2018-09-13 12:40:39 -0700

    Add a CLI option for providing a path.

--- cli.py
+++ cli.py
@@ -3,8 +3,10 @@ import click
 
 
 @click.command()
-def main():
-    print('Program running...')
+@click.option('--path', '-p', help='The path of the directory to be summarized..')
+def main(path):
+    """Summarize the contents of a given directory."""
+    print('Reading files...')
 
 
 if __name__ == '__main__':
