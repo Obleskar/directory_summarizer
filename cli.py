3d9678a27a9f1f98ba459bbe150c8c78a3e36c8e
Obleskar <obleskar@gmail.com>
2018-09-14 15:55:47 -0700

    Add whitepace after commas (PEP8).

--- cli.py
+++ cli.py
@@ -36,7 +36,7 @@ def main(target_path):
 
 
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
