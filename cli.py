ea6da698a65eb5020836b13acab523010d3c7096
Benjamin <benjamindeuson@gmail.com>
2018-09-14 16:26:58 -0700

    Remove the extra zeros in the Mb divider.

--- cli.py
+++ cli.py
@@ -48,9 +48,9 @@ def main(target_path):
 
 def sizeof_fmt(num, suffix='B'):
     for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
-        if abs(num) < 1024.0:
+        if abs(num) < 1024:
             return "%3.1f%s%s" % (num, unit, suffix)
-        num /= 1024.0
+        num /= 1024
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
