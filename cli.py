a87da5de956ed7d4064194bde2d17c3b5d02c83d
Obleskar <obleskar@gmail.com>
2018-11-01 22:07:03 -0700

    Call the relocated summarize function from cli.py

--- cli.py
+++ cli.py
@@ -1,11 +1,16 @@
 from click import command, option
 
+from main import summarize
+
 
 @command()
 @option('--directory_path', '-p', help='The path of the directory to be summarized.')
 @option('--output_path', '-o', help='Path to where the summary results file should be saved.')
 @option('--dry_run', '-n', is_flag=True, help='Preview program output without generating an output file.')
-
+def get_summary(directory_path, output_path, dry_run):
+    """Get a summary of the provided directory."""
+    summarize(directory_path=directory_path, output_path=output_path, dry_run=dry_run)
+    
 
 if __name__ == '__main__':
-    main()
+    get_summary()
